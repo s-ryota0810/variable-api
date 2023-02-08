2b17709ff04327ff229edf197225d7c1312d3bc7
s-ryota0810 <akaashi0810@gmail.com>
2023-02-08 20:18:44 +0900

    連携

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
 
-root.render(<div className="text-red">Hello World</div>);
+root.render(
+    <div className="text-red">
+        <App />
+    </div>
+);
